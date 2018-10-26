0deb58159524d239b0e05abc1bfca80f09f0495b
James Ludlow <jamesludlow@gmail.com>
2018-10-26 07:50:03 -0500

    Handle BGG API timeouts better, because they're rampant and the API is running at a turtle's pace

--- count_thread_users.rb
+++ count_thread_users.rb
@@ -6,35 +6,64 @@
 # - nokogiri gem
 #
 require "nokogiri"
-require "net/http"
+require "httparty"
 
-url = URI.parse("http://www.boardgamegeek.com/xmlapi2/thread?id=426060")
-response = Net::HTTP.start(url.host, url.port) do |http|
-  http.get(url.path + "?" + url.query)
-end
+id = 426060 # MPS / STP thread
+#id = 710797 # TCAT
 
-if response.code != "200"
-  puts "Something went wrong"
-else
-  usernames = {}
-  doc = Nokogiri::XML(response.body)
-  doc.xpath("//thread/articles/article").each do |article|
-    username = article.attributes["username"].value
-    if usernames.has_key?(username)
-      usernames[username] += 1
-    else
-      usernames[username] = 1
+max_batch_size = 500
+degraded_batch_size = 25
+batch_size = max_batch_size
+last_article_id = 0
+usernames = {}
+
+counter = 1
+article_count = 0
+
+loop do
+  url = "https://www.boardgamegeek.com/xmlapi2/thread?id=#{id}&count=#{batch_size}&minarticleid=#{last_article_id}"
+  puts "Batch #{counter} - articles: #{article_count} - #{url}"
+  response = HTTParty.get(url, timeout: 120)
+
+  if response.code != 200
+    puts "Something went wrong"
+    puts response
+    puts "Pausing 30 seconds"
+    batch_size = degraded_batch_size
+    sleep 30
+   
+    next
+  else
+    doc = Nokogiri::XML(response.body)
+    articles = doc.xpath("//thread/articles/article")
+    if articles == nil || articles.count < 1
+      break
+    end
+
+    article_count += articles.count
+      
+    articles.each do |article|
+      last_article_id = article.attributes["id"].value.to_i + 1  # We don't want to see the last article in the next request
+      username = article.attributes["username"].value
+      if usernames.has_key?(username)
+        usernames[username] += 1
+      else
+        usernames[username] = 1
+      end
     end
   end
-  puts "Total posts: #{usernames.values.inject(0) {|sum, n| sum += n}}"
-  puts "Total unique users: #{usernames.keys.count}"
-  puts "\n-------------------------------------------------------"
-  puts "post count: username"
-  puts "-------------------------------------------------------"
+  counter += 1
+  batch_size = max_batch_size
+end
 
-  usernames.sort {|a,b| b[1] <=> a[1]}.each do |n|
-    puts "#{n[1]}: #{n[0]}"
-  end
+puts "Total posts: #{usernames.values.inject(0) {|sum, n| sum += n}}"
+puts "Total unique users: #{usernames.keys.count}"
+puts "\n-------------------------------------------------------"
+puts "post count: username"
+puts "-------------------------------------------------------"
+
+usernames.sort {|a,b| b[1] <=> a[1]}.each do |n|
+  puts "#{n[1]}: #{n[0]}"
 end
 
 
